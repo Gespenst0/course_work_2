580f0b4943332c3910536f7ecc1db15f963436c8
Gespenst0 <101336424+Gespenst0@users.noreply.github.com>
2022-04-01 13:10:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 from main.views import main_blueprint
 from search.views import search_blueprint
+from api.views import api_blueprint
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
 
 app.register_blueprint(main_blueprint)
 app.register_blueprint(search_blueprint)
+app.register_blueprint(api_blueprint)
 
-app.run()
+if __name__ == "__main__":
+    app.run()
